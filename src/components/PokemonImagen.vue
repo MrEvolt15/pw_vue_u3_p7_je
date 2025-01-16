db70de4b7186c8b067f06f87ad3dc40411cd689f
Joel2231 <64055627+MrEvolt15@users.noreply.github.com>
2025-01-16 15:31:38 -0500

    Taller21

---
+++ src/components/PokemonImagen.vue
@@ -0,0 +1,10 @@
+<template>
+<h1>Componente Pokemon Image</h1>
+  
+</template>
+<script>
+export default {
+}
+</script>
+<style>
+</style>
---
+++ src/components/PokemonOpciones.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Componente Pokemon Opciones</h1>
+</template>
+<script>
+export default {
+}
+</script>
+<style>
+</style>
