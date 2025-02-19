b2b6b849f7e47a1b20e3fc596fe5e5f5f513aed7
Joel2231 <64055627+MrEvolt15@users.noreply.github.com>
2025-02-19 12:06:14 -0500

    Tarea 14

--- src/client/PersonaClient.js
+++ src/client/PersonaClient.js
@@ -9,22 +9,42 @@ const obtenerPorId = async (id) => {
 }
 const insertar = async (persona) => {
     //http://localhost:8080/matriculaAPI/v1.1/personas
-    axios.post(`http://localhost:8081/matriculaAPI/v1.1/personas`, persona).then(r => r.data);
+    axios.post(`http://localhost:8081/matriculaAPI/v1.1/personas`, persona).then(r => r.data).then(Response => {
+        console.log(Response);
+        console.log("Persona insertada con exito");
+    }).catch(error => {
+        console.log(error);
+    });
 
 }
 const actualizarCompleto = async (id,persona) => {
     //http://localhost:8080/matriculaAPI/v1.1/personas
-    axios.put(`http://localhost:8081/matriculaAPI/v1.1/personas/${id}`, persona).then(r => r.data);
+    axios.put(`http://localhost:8081/matriculaAPI/v1.1/personas/${id}`, persona).then(r => r.data).then(Response => {
+        console.log(Response);
+        console.log("Persona actualizada completo con exito");
+    }).catch(error => {
+        console.log(error);
+    });
 
 }
 const actualizarParcial = async (id,persona) => {
     //http://localhost:8080/matriculaAPI/v1.1/personas
-    axios.patch(`http://localhost:8081/matriculaAPI/v1.1/personas/${id}`, persona).then(r => r.data);
+    axios.patch(`http://localhost:8081/matriculaAPI/v1.1/personas/${id}`, persona).then(r => r.data).then(Response => {
+        console.log(Response);
+        console.log("Persona actualizada parcial con exito");
+    }).catch(error => {
+        console.log(error);
+    });
 
 }
 const eliminar = async (id) => {
     //http://localhost:8080/matriculaAPI/v1.1/personas
-    axios.delete(`http://localhost:8081/matriculaAPI/v1.1/personas/${id}`).then(r => r.data);
+    axios.delete(`http://localhost:8081/matriculaAPI/v1.1/personas/${id}`).then(r => r.data).then(Response => {
+        console.log(Response);
+        console.log("Persona eliminada con exito");
+    }).catch(error => {
+        console.log(error);
+    });
 
 }
  
@@ -35,3 +55,12 @@ export const insertarFachada = async (persona) => {
     await insertar(persona);
 }
  
+export const actualizarCompletoFachada = async (id,persona) => {
+    await actualizarCompleto(id,persona);
+}
+export const actualizarParcialFachada = async (id,persona) => {
+    await actualizarParcial(id,persona);
+}
+export const eliminarFachada = async (id) => {
+    await eliminar(id);
+}
--- src/components/FormularioComponente.vue
+++ src/components/FormularioComponente.vue
@@ -1,24 +1,35 @@
 <template>
   <h1>Formulario Persona</h1>
-  
-    <label for="id">Id:</label>
-    <input type="text" id="id" v-model="id" />
 
-    <label for="nombre">Nombre:</label>
-    <input type="text" id="nombre" v-model="nombre" />
+  <label for="id">Id:</label>
+  <input type="text" id="id" v-model="id" />
 
-    <label for="apellido">Apellido:</label>
-    <input type="text" id="apellido" v-model="apellido" />
+  <label for="nombre">Nombre:</label>
+  <input type="text" id="nombre" v-model="nombre" />
 
-    <label for="fecha">Fecha Nacimiento:</label>
-    <input type="date" id="fecha" v-model="fecha" />
-  
-  <button @click="consultar()">Consultar</button>
-  <button @click="guardar()">Guardar</button>
+  <label for="apellido">Apellido:</label>
+  <input type="text" id="apellido" v-model="apellido" />
+
+  <label for="fecha">Fecha Nacimiento:</label>
+  <input type="date" id="fecha" v-model="fecha" />
+
+  <div class="botones">
+    <button @click="consultar()">Consultar</button>
+    <button @click="guardar()">Guardar</button>
+    <button @click="actualizar()">Actualizar</button>
+    <button @click="actualizarParcial()">Actualizar Parcial</button>
+    <button @click="eliminar()">Elimniar</button>
+  </div>
 </template>
    
   <script>
-import { obtenerPorIdFachada, insertarFachada } from "@/client/PersonaClient.js";
+import {
+  obtenerPorIdFachada,
+  insertarFachada,
+  actualizarCompletoFachada,
+  actualizarParcialFachada,
+  eliminarFachada
+} from "@/client/PersonaClient.js";
 export default {
   data() {
     return {
@@ -40,14 +51,33 @@ export default {
       this.apellido = data.apellido;
       this.fecha = data.fechaNacimiento;
     },
-    async guardar(){
-      const bodyPersona= {
+    async guardar() {
+      const bodyPersona = {
         nombre: this.nombre,
         apellido: this.apellido,
-        fechaNacimiento: this.fecha
-      }
+        fechaNacimiento: this.fecha,
+      };
       await insertarFachada(bodyPersona);
-    }
+    },
+    async actualizar() {
+      const bodyPersona = {
+        nombre: this.nombre,
+        apellido: this.apellido,
+        fechaNacimiento: this.fecha,
+      };
+      await actualizarCompletoFachada(this.id, bodyPersona);
+    },
+    async actualizarParcial() {
+      const bodyPersona = {
+        nombre: this.nombre,
+        apellido: this.apellido,
+        fechaNacimiento: this.fecha,
+      };
+      await actualizarParcialFachada(this.id, bodyPersona);
+    },
+    async eliminar() {
+      await eliminarFachada(this.id);
+    },
   },
 };
 </script>
@@ -66,7 +96,13 @@ label {
   display: block;
   margin: 10px 0 5px;
 }
-
+.botones {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+}
 input[type="text"],
 input[type="date"] {
   width: 100%;
@@ -83,6 +119,7 @@ button {
   border: none;
   border-radius: 5px;
   cursor: pointer;
+  margin: 5px;
 }
 
 button:hover {
